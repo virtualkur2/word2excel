036d9f8354c88815576c62fc341b04c5a4b74c7c
Mauricio Contreras <virtual.kur2@gmail.com>
2020-03-21 18:46:56 +0100

    functions commented

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ const path = require('path');
 const wordParser = require('word-text-parser');
 const ExcelJS = require('exceljs');
 
+
+/* La siguiente función se encarga de crear un documento de Excel
+* a partir de una lista de datos recibida como parámetro
+*
+* @input dataList [Array]: Array con datos: { PU: <cadena>, Alta: <cadena>, Fecha: <cadena>}
+*/
 const createWorkBook = function(dataList) {
+  if(!dataList || !Array.isArray(dataList) || !dataList.length) return;
   let wb = new ExcelJS.Workbook();
   wb.created = new Date();
   wb.creator = 'Someone';
@@ -12,9 +19,13 @@ const createWorkBook = function(dataList) {
       firstSheet: 0, activeTab: 1, visibility: 'visible'
     }
   ]
+  //creamos la hoja del libro
   let dataSheet = wb.addWorksheet('Data');
+
   //20 campos por registro, es lo que estableces en tu pregunta
-  // los 3 datos van las columnas 3, 5 y 7. con PU, alta y fecha como orden.
+  // los 3 datos van en las columnas 3, 5 y 7. con PU, alta y fecha respectivamente.
+
+  // creamos la cabecera de datos
   let columnHeaders = [];
   for(let i = 0; i < 20; i++) {
     let header = {header: `Dato${i}`, key: `Dato${i}`, width: 30};
@@ -33,45 +44,78 @@ const createWorkBook = function(dataList) {
     columnHeaders.push(header);
   }
   dataSheet.columns = columnHeaders;
+
+  // Recorremos la lista y almacenamos los registros en la hoja
   dataList.forEach(data => {
     dataSheet.addRow({PU: data.PU, Alta: data.Alta, Fecha: data.Fecha});
   });
-  // verificar el nombre de salida del archivo
+
+  // Escribimos el archivo resultante:
   wb.xlsx.writeFile(path.join(__dirname,'test.xlsx'))
   .then(() => {
     console.log('Done');
   })
+  .catch(error => {
+    console.error(`Algo salió mal: ${error.message}`);
+  });
 }
 
+/*
+* La siguiente función analiza los párrafos capturados del archivo de Word
+* y devuelve una lista con los datos obtenidos de los mismos
+* @input resultsList [Array]: Array con cadenas correspondientes a cada párrafo del archivo
+* @output data [Array]: Array con datos: { PU: <cadena>, Alta: <cadena>, Fecha: <cadena>}
+*/
 const parseResult = function(resultsList) {
+  if(!resultsList || !Array.isArray(resultsList) || !resultsList.length) return;
+
+  // filtramos los párrafos que contengan "PU." y "Alta"
   const filteredParagraphs = resultsList.filter(p => p.includes('PU.') && p.includes('Alta'));
+
+  // Array que almacenará los datos extraídos
   const data = [];
+
+  // Expresión regular para evaluar la fecha.
   const regexFecha = /^\d\d\/\d\d$/i
+
+  // recorremos cada párrafo
   filteredParagraphs.forEach(p => {
+    //creamos el objeto para almacenar los datos
     let value = {
       PU: '',
       Alta: '',
       Fecha: ''
     }
+
+    // separamos cada párrafo en palabras
     let splitedParagraph = p.split(' ');
+
+    //analizamos cada palabra y capturamos los datos
     splitedParagraph.forEach((word, index) => {
       if(word.includes('PU.')) {
         value['PU'] = word.split('.')[1];
       }
-      if(word.includes('Alta')) {
+      if(word === 'Alta') {
         value['Alta'] = splitedParagraph[index + 1];
       }
       if(regexFecha.test(word)) {
         value['Fecha'] = `${word}/2020`;
       }
     });
+
+    // almacenamos el resultado en el array
     data.push(value);
   });
+
+  // mostramos los datos por consola
   console.log(data);
+
+  // llamamos al procedimiento para crear el libro de Excel a partir de los datos
   createWorkBook(data);
 }
 
-// nombre del archivo de entrada
+// nombre del archivo a analizar
 const fileToParse = path.join(__dirname, 'demo.docx');
 
+// llamamos al analizador del archivo
 wordParser(fileToParse, parseResult);
